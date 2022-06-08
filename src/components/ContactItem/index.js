3d047e788e5a77cf41385cab482c2ae9511d485d
nagarjunanaik <nagarjunanaik2606@gmail.com>
2022-06-08 04:55:11 +0000

    adding

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,34 @@
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, togooleIsFavorite} = props
+  const {id, name, mobileNo, isFavorite} = contactDetails
+  const onClickIsFaveret = () => {
+    togooleIsFavorite(id)
+  }
+
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button
+          type="button"
+          className="favorite-icon-container"
+          onClick={onClickIsFaveret}
+        >
+          <img src={starImgUrl} className="favorite-icon" alt="star" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
